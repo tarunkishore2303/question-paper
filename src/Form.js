a83c975d722408b6d8525f39ec467a0b202226c6
tarunkishore2303 <tarunksu30@gmail.com>
2021-10-15 20:24:53 +0530

    Added PDF generation

--- src/Form.js
+++ src/Form.js
@@ -1,291 +1,286 @@
 import React, { useState } from "react";
-import { PDFDownloadLink } from "react-pdf";
+import { PDFDownloadLink } from "@react-pdf/renderer";
 import GetPDF from "./GetPDF";
-import { useFormik } from "formik";
 import "./form.css";
-import { Field, Formik } from "formik";
+import { useFormik } from "formik";
 
 function Form() {
+	const [answers, setAnswers] = useState({});
+	console.log(answers);
+	const formik = useFormik({
+		initialValues: {
+			qn1: "",
+			qn2: "",
+			qn3: "",
+			qn4: "",
+			qn5: "",
+			qn6: "",
+			qn7: "",
+			qn8: "",
+			qn9: "",
+		},
+		onSubmit: async (values) => {
+			await new Promise((resolve, reject) => {
+				resolve(setAnswers(values));
+				console.log(JSON.stringify(values, null, 2));
+			});
+		},
+	});
 	return (
 		<div className='form'>
 			<h1> THIAGARJAR COLLEGE OF ENGINEERING </h1>
-
 			<div className='form-container'>
-				<Formik
-					initialValues={{
-						qn1: "",
-						qn2: "",
-						qn3: "",
-						qn4: "",
-						qn5: "",
-						qn6: "",
-						qn7: "",
-						qn8: "",
-						qn9: "",
-					}}
-					onSubmit={async (values) => {
-						await new Promise((r) => setTimeout(r, 500));
-						alert(JSON.stringify(values, null, 2));
-					}}
-				>
-					{({ values }) => (
-						<form>
-							<fieldset>
-								<legend>COURSE NAME</legend>
+				<form onSubmit={formik.handleSubmit}>
+					<fieldset>
+						<legend>COURSE NAME</legend>
 
-								<p>
-									1. Questions are clear and defenite without
-									ambiguties
-								</p>
-								<div
-									role='group'
-									aria-labelledby='my-radio-group'
-								>
-									 {" "}
-									<Field
-										type='radio'
-										id='Yes'
-										name='qn1'
-										value='Yes'
-										required
-									/>
-									  <label for='Yes'>Yes</label>
-									 {" "}
-									<Field
-										type='radio'
-										id='No'
-										name='qn1'
-										value='No'
-										required
-									/>
-									  <label for='Yes'>No</label>
-								</div>
-								{values.qn1}
+						<p>
+							1. Questions are clear and defenite without
+							ambiguties
+						</p>
+						<div role='group' aria-labelledby='my-radio-group'>
+							 {" "}
+							<input
+								type='radio'
+								id='Yes'
+								name='qn1'
+								value='Yes'
+								required
+								onChange={formik.handleChange}
+							/>
+							  <label for='Yes'>Yes</label> {" "}
+							<input
+								type='radio'
+								id='No'
+								name='qn1'
+								value='No'
+								required
+								onChange={formik.handleChange}
+							/>
+							  <label for='Yes'>No</label>
+						</div>
+						<p>
+							2. Course code, course name, date and session are
+							given as per CAT timetable
+						</p>
+						<div role='group' aria-labelledby='my-radio-group'>
+							 {" "}
+							<input
+								type='radio'
+								id='Yes'
+								name='qn2'
+								value='Yes'
+								required
+								onChange={formik.handleChange}
+							/>
+							  <label for='Yes'>Yes</label> {" "}
+							<input
+								type='radio'
+								id='No'
+								name='qn2'
+								value='No'
+								required
+								onChange={formik.handleChange}
+							/>
+							  <label for='No'>No</label>
+						</div>
 
-								<p>
-									2. Course code, course name, date and
-									session are given as per CAT timetable
-								</p>
-								<div
-									role='group'
-									aria-labelledby='my-radio-group'
-								>
-									 {" "}
-									<Field
-										type='radio'
-										id='Yes'
-										name='qn2'
-										value='Yes'
-										required
-									/>
-									  <label for='Yes'>Yes</label>
-									 {" "}
-									<Field
-										type='radio'
-										id='No'
-										name='qn2'
-										value='No'
-										required
-									/>
-									  <label for='No'>No</label>
-								</div>
-								{values.qn2}
+						<p>
+							3. Use of appropriate keyword for the revised Bloom
+							level
+						</p>
+						<div role='group' aria-labelledby='my-radio-group'>
+							 {" "}
+							<input
+								type='radio'
+								id='Yes'
+								name='qn3'
+								value='Yes'
+								required
+								onChange={formik.handleChange}
+							/>
+							  <label for='Yes'>Yes</label> {" "}
+							<input
+								type='radio'
+								id='No'
+								name='qn3'
+								value='No'
+								required
+								onChange={formik.handleChange}
+							/>
+							  <label for='No'>No</label>
+						</div>
 
-								<p>
-									3. Use of appropriate keyword for the
-									revised Bloom level
-								</p>
-								<div
-									role='group'
-									aria-labelledby='my-radio-group'
-								>
-									 {" "}
-									<Field
-										type='radio'
-										id='Yes'
-										name='qn3'
-										value='Yes'
-										required
-									/>
-									  <label for='Yes'>Yes</label>
-									 {" "}
-									<Field
-										type='radio'
-										id='No'
-										name='qn3'
-										value='No'
-										required
-									/>
-									  <label for='No'>No</label>
-								</div>
-								{values.qn3}
+						<p>
+							4. Question paper is set for the Maximum marks
+							allotted
+						</p>
+						<div role='group' aria-labelledby='my-radio-group'>
+							 {" "}
+							<input
+								type='radio'
+								id='Yes'
+								name='qn4'
+								value='Yes'
+								onChange={formik.handleChange}
+							/>
+							  <label for='Yes'>Yes</label> {" "}
+							<input
+								type='radio'
+								id='No'
+								name='qn4'
+								value='No'
+								onChange={formik.handleChange}
+							/>
+							  <label for='No'>No</label>
+						</div>
 
-								<p>
-									4. Question paper is set for the Maximum
-									marks allotted
-								</p>
-								<div
-									role='group'
-									aria-labelledby='my-radio-group'
-								>
-									 {" "}
-									<Field
-										type='radio'
-										id='Yes'
-										name='qn4'
-										value='Yes'
-									/>
-									  <label for='Yes'>Yes</label>
-									 {" "}
-									<Field
-										type='radio'
-										id='No'
-										name='qn4'
-										value='No'
-									/>
-									  <label for='No'>No</label>
-								</div>
+						<p>
+							5. Correctness of categorization of each Question as
+							per CO list given in the Syllabus
+						</p>
+						<div role='group' aria-labelledby='my-radio-group'>
+							 {" "}
+							<input
+								type='radio'
+								id='Yes'
+								name='qn5'
+								onChange={formik.handleChange}
+								value='Yes'
+							/>
+							  <label for='Yes'>Yes</label> {" "}
+							<input
+								type='radio'
+								id='No'
+								name='qn5'
+								value='No'
+								onChange={formik.handleChange}
+							/>
+							  <label for='No'>No</label>
+						</div>
 
-								<p>
-									5. Correctness of categorization of each
-									Question as per CO list given in the
-									Syllabus
-								</p>
-								<div
-									role='group'
-									aria-labelledby='my-radio-group'
-								>
-									 {" "}
-									<Field
-										type='radio'
-										id='Yes'
-										name='qn5'
-										value='Yes'
-									/>
-									  <label for='Yes'>Yes</label>
-									 {" "}
-									<Field
-										type='radio'
-										id='No'
-										name='qn5'
-										value='No'
-									/>
-									  <label for='No'>No</label>
-								</div>
+						<p>
+							6. Consistency of mapping of each question w.r.t
+							revised Bloom&#39;s level and the CO
+						</p>
+						<div role='group' aria-labelledby='my-radio-group'>
+							 {" "}
+							<input
+								type='radio'
+								id='Yes'
+								name='qn6'
+								value='Yes'
+								onChange={formik.handleChange}
+							/>
+							  <label for='Yes'>Yes</label> {" "}
+							<input
+								type='radio'
+								id='No'
+								name='qn6'
+								value='No'
+								onChange={formik.handleChange}
+							/>
+							  <label for='No'>No</label>
+						</div>
 
-								<p>
-									6. Consistency of mapping of each question
-									w.r.t revised Bloom&#39;s level and the CO
-								</p>
-								<div
-									role='group'
-									aria-labelledby='my-radio-group'
-								>
-									 {" "}
-									<Field
-										type='radio'
-										id='Yes'
-										name='qn6'
-										value='Yes'
-									/>
-									  <label for='Yes'>Yes</label>
-									 {" "}
-									<Field
-										type='radio'
-										id='No'
-										name='qn6'
-										value='No'
-									/>
-									  <label for='No'>No</label>
-								</div>
+						<p>
+							7. Correctness weightage of each question as per
+							assessment pattern
+						</p>
+						<div role='group' aria-labelledby='my-radio-group'>
+							 {" "}
+							<input
+								type='radio'
+								id='Yes'
+								name='qn7'
+								value='Yes'
+								onChange={formik.handleChange}
+							/>
+							  <label for='Yes'>Yes</label> {" "}
+							<input
+								type='radio'
+								id='No'
+								name='qn7'
+								value='No'
+								onChange={formik.handleChange}
+							/>
+							  <label for='No'>No</label>
+						</div>
 
-								<p>
-									7. Correctness weightage of each question as
-									per assessment pattern
-								</p>
-								<div
-									role='group'
-									aria-labelledby='my-radio-group'
-								>
-									 {" "}
-									<Field
-										type='radio'
-										id='Yes'
-										name='qn7'
-										value='Yes'
-									/>
-									  <label for='Yes'>Yes</label>
-									 {" "}
-									<Field
-										type='radio'
-										id='No'
-										name='qn7'
-										value='No'
-									/>
-									  <label for='No'>No</label>
-								</div>
+						<p>
+							8. Split-up marks for each sub-divisions are given
+							correctly in the Question paper
+						</p>
+						<div role='group' aria-labelledby='my-radio-group'>
+							 {" "}
+							<input
+								type='radio'
+								id='Yes'
+								name='qn8'
+								value='Yes'
+								onChange={formik.handleChange}
+							/>
+							  <label for='Yes'>Yes</label> {" "}
+							<input
+								type='radio'
+								id='No'
+								name='qn8'
+								value='No'
+								onChange={formik.handleChange}
+							/>
+							  <label for='No'>No</label>
+						</div>
 
-								<p>
-									8. Split-up marks for each sub-divisions are
-									given correctly in the Question paper
-								</p>
-								<div
-									role='group'
-									aria-labelledby='my-radio-group'
-								>
-									 {" "}
-									<Field
-										type='radio'
-										id='Yes'
-										name='qn8'
-										value='Yes'
-									/>
-									  <label for='Yes'>Yes</label>
-									 {" "}
-									<Field
-										type='radio'
-										id='No'
-										name='qn8'
-										value='No'
-									/>
-									  <label for='No'>No</label>
-								</div>
+						<p>9. Coverage of syllabus </p>
+						<div role='group' aria-labelledby='my-radio-group'>
+							 {" "}
+							<input
+								type='radio'
+								id='Yes'
+								name='qn9'
+								value='Yes'
+								onChange={formik.handleChange}
+							/>
+							  <label for='Yes'>Yes</label> {" "}
+							<input
+								type='radio'
+								id='No'
+								name='qn9'
+								value='No'
+								onChange={formik.handleChange}
+							/>
+							  <label for='No'>No</label>
+						</div>
 
-								<p>9. Coverage of syllabus </p>
-								<div
-									role='group'
-									aria-labelledby='my-radio-group'
-								>
-									 {" "}
-									<Field
-										type='radio'
-										id='Yes'
-										name='qn9'
-										value='Yes'
-									/>
-									  <label for='Yes'>Yes</label>
-									 {" "}
-									<Field
-										type='radio'
-										id='No'
-										name='qn9'
-										value='No'
-									/>
-									  <label for='No'>No</label>
-								</div>
+						<p>10. Other Remarks :</p>
+						<textarea
+							rows='5'
+							cols='50'
+							onChange={formik.handleChange}
+						></textarea>
 
-								<p>10. Other Remarks :</p>
-								<textarea rows='5' cols='50'></textarea>
-
-								<p>
-									<button type='submit' class='btn'>
-										Submit{" "}
-									</button>
-								</p>
-							</fieldset>
-						</form>
-					)}
-				</Formik>
+						<p>
+							<button type='submit' class='btn'>
+								{formik.isSubmitting ? (
+									<div className='loader'></div>
+								) : (
+									<PDFDownloadLink
+										document={<GetPDF answers={answers} />}
+										// data={formik.values}
+										fileName='InfoSec_2021'
+										className='downlad-btn'
+									>
+										{({ blob, url, loading, error }) =>
+											loading
+												? "Loading document..."
+												: "Download"
+										}
+									</PDFDownloadLink>
+								)}
+							</button>
+						</p>
+					</fieldset>
+				</form>
 			</div>
 		</div>
 	);
--- src/GetPDF.js
+++ src/GetPDF.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Document, Page, Text, View } from "@react-pdf/renderer";
+import { Document, Page, Text, View, StyleSheet } from "@react-pdf/renderer";
 
 var today = new Date();
 var dd = String(today.getDate()).padStart(2, "0");
@@ -7,18 +7,93 @@ var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
 var yyyy = today.getFullYear();
 
 today = dd + "/" + mm + "/" + yyyy;
-
-function GetPDF({ data, styles }) {
+const styles = StyleSheet.create({
+	page: {
+		flexDirection: "column",
+		padding: 20,
+		fontFamily: "Helvetica",
+		fontSize: 12,
+		backgroundColor: "#fff",
+	},
+	header: {
+		flexDirection: "column",
+		marginBottom: 20,
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: "bold",
+		marginBottom: 10,
+	},
+	subtitle: {
+		fontSize: 16,
+		fontWeight: "bold",
+		marginBottom: 10,
+	},
+	date: {
+		fontSize: 12,
+		marginBottom: 10,
+	},
+	body: {
+		flexDirection: "column",
+		marginBottom: 20,
+	},
+	bodyText: {
+		fontSize: 12,
+		marginBottom: 10,
+	},
+});
+function GetPDF({ answers }) {
+	console.log(answers);
 	return (
 		<Document>
 			<Page size='A4' style={styles.page}>
 				<View style={styles.header}>
-					<Text style={styles.title}>{data.title}</Text>
-					<Text style={styles.subtitle}>{data.subtitle}</Text>
 					<Text style={styles.date}>{today}</Text>
+					<Text style={styles.title}>Question Paper Audit</Text>
 				</View>
 				<View style={styles.body}>
-					<Text style={styles.bodyText}>{data.body}</Text>
+					<Text style={styles.subtitle}>
+						1. Questions are clear and defenite without ambiguties
+					</Text>
+					<Text style={styles.bodyText}>{answers["qn1"]}</Text>
+					<Text style={styles.subtitle}>
+						2. Course code, course name, date and session are given
+						as per CAT timetable
+					</Text>
+					<Text style={styles.bodyText}>{answers["qn2"]}</Text>
+					<Text style={styles.subtitle}>
+						3. Use of appropriate keyword for the revised Bloom
+						level
+					</Text>
+					<Text style={styles.bodyText}>{answers["qn3"]}</Text>
+					<Text style={styles.subtitle}>
+						4. Question paper is set for the Maximum marks allotted
+					</Text>
+					<Text style={styles.bodyText}>{answers["qn4"]}</Text>
+					<Text style={styles.subtitle}>
+						5. Correctness of categorization of each Question as per
+						CO list given in the Syllabus
+					</Text>
+					<Text style={styles.bodyText}>{answers["qn5"]}</Text>
+					<Text style={styles.subtitle}>
+						6. Consistency of mapping of each question w.r.t revised
+						Bloom&#39;s level and the CO
+					</Text>
+					<Text style={styles.bodyText}>{answers["qn6"]}</Text>
+					<Text style={styles.subtitle}>
+						7. Correctness weightage of each question as per
+						assessment pattern
+					</Text>
+					<Text style={styles.bodyText}>{answers["qn7"]}</Text>
+					<Text style={styles.subtitle}>
+						8. Split-up marks for each sub-divisions are given
+						correctly in the Question paper
+					</Text>
+					<Text style={styles.bodyText}>{answers["qn8"]}</Text>
+					<Text style={styles.subtitle}>9. Coverage of syllabus</Text>
+					<Text style={styles.bodyText}>{answers["qn9"]}</Text>
+					<Text style={styles.subtitle}>10. Other Remarks :</Text>
+					<Text style={styles.bodyText}>{answers["qn10"]}</Text>
 				</View>
 			</Page>
 		</Document>
